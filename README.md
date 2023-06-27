8ba6272d8dbfb87ee1d3dcc26f13acfa2dc3ba95
Hein du Plessis <hein@aerobots.co.za>
2023-06-27 18:08:17 +0200

    secrets in env varas

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # sunsynk_alerts
 
+## requirements:
+
 pip install aiohttp
 pip install requests
+
+## Envorment Variables:
+SUNSYNK_USER
+SUNSYNK_PASS
+BULKSMS_USER
+BULKSMS_PASS
+
+### Set with Windows:
+Set-Item -Path "env:SUNSYNK_USER" -Value "hein@aerobots.co.za" -Force
+Set-Item -Path "env:SUNSYNK_PASS" -Value [pwd] -Force
+Set-Item -Path "env:BULKSMS_USER" -Value "aerobots" -Force
+Set-Item -Path "env:BULKSMS_PASS" -Value [pwd] -Force
+
+Confirm:
+gci env:* | sort-object name
--- monitor_inverter.py
+++ monitor_inverter.py
@@ -1,3 +1,7 @@
+# Pulls realtime feed from inverter cloud and sends out sms alerts if critcal
+# Based on https://github.com/jamesridgway/sunsynk-api-client
+# June 2023, Hein du Plessis
+
 import asyncio
 import os
 import requests
@@ -8,10 +12,7 @@ import logging
 
 from sunsynk.client import SunsynkClient
 
-#https://api.bulksms.com/v1/messages/send?to=%2b27824616593&body=Hello%20World
-#aerobots / wwwBulksms1
-
-APP_VERSION='Sunsynk Monitor V1.02'
+APP_VERSION='Sunsynk Monitor V0.03'
 
 BATT_SOC_LOW_THRESHOLD=50
 BATT_SOC_LOW_CRIT_THRESHOLD=20
@@ -26,12 +27,19 @@ BATT_SOC_ALARM_CRITICAL_FN="battery_soc_alarm_critical.sig"
 BATT_PWR_ALARM_FN="battery_pwr_alarm.sig"
 BATT_PWR_ALARM_CRITICAL_FN="battery_pwr_alarm_critical.sig"
 
+#Init enviroment variable holders
+SUNSYNK_USER=os.getenv('SUNSYNK_USER')
+SUNSYNK_PASS=os.getenv('SUNSYNK_PASS')
+BULKSMS_USER=os.getenv('BULKSMS_USER')
+BULKSMS_PASS=os.getenv('BULKSMS_PASS')
+
+#Make this false if you're using a cron to run on schedule
 infinite_loop = True
 
 def send_sms(sms_message):
     # HTTP Basic Authentication credentials
-    username = "aerobots"
-    password = "wwwBulksms1"
+    # username = "aerobots"
+    # https://api.bulksms.com/v1/messages/send?to=%2b27824616593&body=Hello%20World
 
     # API endpoint URL
     url = "https://api.bulksms.com/v1/messages"
@@ -48,7 +56,7 @@ def send_sms(sms_message):
     # Convert the parameters to JSON format
     json_params = json.dumps(params)
 
-    response = requests.post(url, data=json_params, headers=headers, auth=(username, password))
+    response = requests.post(url, data=json_params, headers=headers, auth=(BULKSMS_USER, BULKSMS_PASS))
     # Check the response status code
     if response.status_code in (200,201):
         # Request was successful
@@ -72,14 +80,13 @@ def check_alarm(condition_alarm, condition_reset, sig_file, msg):
         print(f"Alarm reset ({sig_file})")
 
 async def main():
-    sunsynk_username = "hein@aerobots.co.za"
-    sunsynk_password = "wwwSunsynk"
+    # sunsynk_username = "hein@aerobots.co.za"
     check_count=0
 
     print(APP_VERSION)
     logging.info(APP_VERSION)
 
-    async with SunsynkClient(sunsynk_username, sunsynk_password) as client:
+    async with SunsynkClient(SUNSYNK_USER, SUNSYNK_PASS) as client:
         inverters = await client.get_inverters()
         for inverter in inverters:
             while infinite_loop:
@@ -116,4 +123,16 @@ logging.basicConfig(
     filename='monitor_inverter.log'
 )
 
-asyncio.run(main())
+#Confirm variables are set
+ok_to_go=True
+if (SUNSYNK_USER == None) or (SUNSYNK_PASS == None) or (BULKSMS_USER == None) or (BULKSMS_PASS == None):
+    print("Please set enviroment variables first (see readme)")
+    ok_to_go=False
+
+if ok_to_go:
+    print(f"using SUNSYNK_USER={SUNSYNK_USER} and BULKSMS_USER={BULKSMS_USER}")
+
+# ok_to_go=False
+
+if ok_to_go:
+    asyncio.run(main())
